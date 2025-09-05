6256639492565f7c8fb0c92e75e44527a64d71fb
Emanuele Torrisi <veegreb@gmail.com>
2025-09-05 12:36:15 +0200

    Modified the who we are

--- pages/Who_we_are.py
+++ pages/Who_we_are.py
@@ -1,3 +1,56 @@
 import streamlit as st
 
-st.markdown("We are a group of LeWagon's students (https://www.linkedin.com/in/emanuele-torrisi-08a3572a4)")
+st.markdown("""
+<div style="font-family: 'Garamond', serif; font-style: italic; text-align: center; line-height: 1.6;">
+
+<!-- Emanuele Torrisi -->
+<div style="font-size: 28px; margin-bottom: 40px;">
+    <span style="display: inline-block; width: 20px; height: 20px; background-color: #FFC30B; border-radius: 50%; margin-right: 10px; vertical-align: middle;"></span>
+    <span style="color: white;">Hi! My name is </span><span style="color: #FFC30B;">Emanuele Torrisi</span><br>
+    <span style="font-size: 24px; color: white;">
+    bla bla bla bla bla bla bla bla bla bla<br>
+    bla bla bla bla bla bla bla bla bla bla<br>
+    bla bla bla bla bla bla bla bla bla bla<br>
+    bla bla bla bla bla bla bla bla bla bla.<br>
+    Let's talk! <a href="https://www.linkedin.com/in/emanuele-torrisi-08a3572a4" target="_blank" style="color: white; text-decoration: underline;">LinkedIn</a>
+    </span>
+</div>
+
+<!-- Katherine Stewart -->
+<div style="font-size: 28px; margin-bottom: 40px;">
+    <span style="display: inline-block; width: 20px; height: 20px; background-color: #FF69B4; border-radius: 50%; margin-right: 10px; vertical-align: middle;"></span>
+    <span style="color: white;">Hi! My name is </span><span style="color: #FF69B4;">Katherine Stewart</span><br>
+    <span style="font-size: 24px; color: white;">
+    bla bla bla bla bla bla bla bla bla bla<br>
+    bla bla bla bla bla bla bla bla bla bla<br>
+    bla bla bla bla bla bla bla bla bla bla<br>
+    bla bla bla bla bla bla bla bla bla bla
+    </span>
+</div>
+
+<!-- Rohan Raghava Poojary -->
+<div style="font-size: 28px; margin-bottom: 40px;">
+    <span style="display: inline-block; width: 20px; height: 20px; background-color: #32CD32; border-radius: 50%; margin-right: 10px; vertical-align: middle;"></span>
+    <span style="color: white;">Hi! My name is </span><span style="color: #32CD32;">Rohan Raghava Poojary</span><br>
+    <span style="font-size: 24px; color: white;">
+    bla bla bla bla bla bla bla bla bla bla<br>
+    bla bla bla bla bla bla bla bla bla bla<br>
+    bla bla bla bla bla bla bla bla bla bla<br>
+    bla bla bla bla bla bla bla bla bla bla
+    </span>
+</div>
+
+<!-- Thahyra van Heyningen -->
+<div style="font-size: 28px; margin-bottom: 40px;">
+    <span style="display: inline-block; width: 20px; height: 20px; background-color: #800080; border-radius: 50%; margin-right: 10px; vertical-align: middle;"></span>
+    <span style="color: white;">Hi! My name is </span><span style="color: #800080;">Thahyra van Heyningen</span><br>
+    <span style="font-size: 24px; color: white;">
+    bla bla bla bla bla bla bla bla bla bla<br>
+    bla bla bla bla bla bla bla bla bla bla<br>
+    bla bla bla bla bla bla bla bla bla bla<br>
+    bla bla bla bla bla bla bla bla bla bla
+    </span>
+</div>
+
+</div>
+""", unsafe_allow_html=True)
