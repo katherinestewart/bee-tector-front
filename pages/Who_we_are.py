5b338d6a58b932f3aaace91dc271038a309d5bac
Emanuele Torrisi <veegreb@gmail.com>
2025-09-05 10:36:04 +0200

    I created the quiz, modified whoweare and requirements

---
+++ pages/Who_we_are.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.markdown("We are a group of LeWagon's students (https://www.linkedin.com/in/emanuele-torrisi-08a3572a4)")
--- pages/whoweare.py
+++
@@ -1,3 +0,0 @@
-import streamlit as st
-
-st.markdown("We are a group of LeWagon's students")
