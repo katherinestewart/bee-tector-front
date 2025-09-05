0839eb66ce362a0cb2c2c249fad4430ad7dfaa46
Emanuele Torrisi <veegreb@gmail.com>
2025-09-05 16:26:03 +0200

    Modified the font color

--- pages/Who_we_are.py
+++ pages/Who_we_are.py
@@ -6,21 +6,21 @@ st.markdown("""
 <!-- Emanuele Torrisi -->
 <div style="font-size: 28px; margin-bottom: 40px;">
     <span style="display: inline-block; width: 20px; height: 20px; background-color: #FFC30B; border-radius: 50%; margin-right: 10px; vertical-align: middle;"></span>
-    <span style="color: white;">Hi! My name is </span><span style="color: #FFC30B;">Emanuele Torrisi</span><br>
-    <span style="font-size: 24px; color: white;">
+    <span style="color: black;">Hi! My name is </span><span style="color: #FFC30B;">Emanuele Torrisi</span><br>
+    <span style="font-size: 24px; color: black;">
     bla bla bla bla bla bla bla bla bla bla<br>
     bla bla bla bla bla bla bla bla bla bla<br>
     bla bla bla bla bla bla bla bla bla bla<br>
     bla bla bla bla bla bla bla bla bla bla.<br>
-    Let's talk! <a href="https://www.linkedin.com/in/emanuele-torrisi-08a3572a4" target="_blank" style="color: white; text-decoration: underline;">LinkedIn</a>
+    Let's talk! <a href="https://www.linkedin.com/in/emanuele-torrisi-08a3572a4" target="_blank" style="color: black; text-decoration: underline;">LinkedIn</a>
     </span>
 </div>
 
 <!-- Katherine Stewart -->
 <div style="font-size: 28px; margin-bottom: 40px;">
     <span style="display: inline-block; width: 20px; height: 20px; background-color: #FF69B4; border-radius: 50%; margin-right: 10px; vertical-align: middle;"></span>
-    <span style="color: white;">Hi! My name is </span><span style="color: #FF69B4;">Katherine Stewart</span><br>
-    <span style="font-size: 24px; color: white;">
+    <span style="color: black;">Hi! My name is </span><span style="color: #FF69B4;">Katherine Stewart</span><br>
+    <span style="font-size: 24px; color: black;">
     bla bla bla bla bla bla bla bla bla bla<br>
     bla bla bla bla bla bla bla bla bla bla<br>
     bla bla bla bla bla bla bla bla bla bla<br>
@@ -31,8 +31,8 @@ st.markdown("""
 <!-- Rohan Raghava Poojary -->
 <div style="font-size: 28px; margin-bottom: 40px;">
     <span style="display: inline-block; width: 20px; height: 20px; background-color: #32CD32; border-radius: 50%; margin-right: 10px; vertical-align: middle;"></span>
-    <span style="color: white;">Hi! My name is </span><span style="color: #32CD32;">Rohan Raghava Poojary</span><br>
-    <span style="font-size: 24px; color: white;">
+    <span style="color: black;">Hi! My name is </span><span style="color: #32CD32;">Rohan Raghava Poojary</span><br>
+    <span style="font-size: 24px; color: black;">
     bla bla bla bla bla bla bla bla bla bla<br>
     bla bla bla bla bla bla bla bla bla bla<br>
     bla bla bla bla bla bla bla bla bla bla<br>
@@ -43,8 +43,8 @@ st.markdown("""
 <!-- Thahyra van Heyningen -->
 <div style="font-size: 28px; margin-bottom: 40px;">
     <span style="display: inline-block; width: 20px; height: 20px; background-color: #800080; border-radius: 50%; margin-right: 10px; vertical-align: middle;"></span>
-    <span style="color: white;">Hi! My name is </span><span style="color: #800080;">Thahyra van Heyningen</span><br>
-    <span style="font-size: 24px; color: white;">
+    <span style="color: black;">Hi! My name is </span><span style="color: #800080;">Thahyra van Heyningen</span><br>
+    <span style="font-size: 24px; color: black;">
     bla bla bla bla bla bla bla bla bla bla<br>
     bla bla bla bla bla bla bla bla bla bla<br>
     bla bla bla bla bla bla bla bla bla bla<br>
